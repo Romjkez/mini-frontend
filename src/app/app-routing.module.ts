3b3f118d48a73749236400b2834144ae65e27498
Romjkez <meshkov.ra@ya.ru>
2021-05-10 19:52:10 +0300

    feat(main-page): create module and component, mark as default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './modules/main-page/components/main-page/main-page.component';
+import { NotFoundPageComponent } from './common/components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: MainPageComponent,
+  },
+  {
+    path: '404',
+    pathMatch: 'full',
+    component: NotFoundPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
