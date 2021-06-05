3cf7c15067e24c6a60f8b2e8aac1321f0bc46229
Romjkez <meshkov.ra@ya.ru>
2021-06-05 20:20:17 +0300

    feat(exercise-form): implement searching of articles and tests

--- src/app/modules/exercises/components/exercise-form/exercise-form.component.ts
+++ src/app/modules/exercises/components/exercise-form/exercise-form.component.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { FormArray, FormGroup } from '@angular/forms';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Article } from '../../../articles/models/article';
 import { SimpleTest } from '../../../tests/models/simple-test';
 import { TestService } from '../../../tests/test.service';
 import { ArticleService } from '../../../articles/article.service';
-import { map, shareReplay } from 'rxjs/operators';
+import { auditTime, map, shareReplay, tap } from 'rxjs/operators';
 import { MenuItem } from 'primeng/api';
+import { Test } from '../../../tests/models/test';
+
+enum ItemType {
+  TEST = 'Тест',
+  ARTICLE = 'Статья'
+}
 
 @Component({
   selector: 'mn-exercise-form',
@@ -17,8 +23,8 @@ import { MenuItem } from 'primeng/api';
 export class ExerciseFormComponent implements OnInit {
   @Input() readonly: boolean;
   @Input() form: FormGroup;
-  articles$: Observable<Array<Article>>;
-  tests$: Observable<Array<SimpleTest>>;
+  articles$: Observable<Array<Article & { disabled: boolean }>>;
+  tests$: Observable<Array<SimpleTest & { disabled: boolean }>>;
   selectedArticle: Array<Article>;
   selectedTest: Array<SimpleTest>;
   articleAddingMode: boolean;
@@ -26,9 +32,13 @@ export class ExerciseFormComponent implements OnInit {
 
   addItemBtnItems: Array<MenuItem> = [];
   items: FormArray;
+  searchTestText: FormControl = this.fb.control('');
+  searchArticleText: FormControl = this.fb.control('');
 
   constructor(private readonly testService: TestService,
-              private readonly articleService: ArticleService) {
+              private readonly articleService: ArticleService,
+              private readonly fb: FormBuilder,
+              private readonly cdr: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
@@ -42,23 +52,80 @@ export class ExerciseFormComponent implements OnInit {
 
     this.articles$ = this.articleService.getMany({})
       .pipe(
-        map(articles => articles.data),
-        shareReplay(1)
+        map(res => res.data),
+        shareReplay(1),
+        map(tests => tests.map(t => Object.assign(t, {disabled: this.isItemSelected(t)}))),
       );
 
     this.tests$ = this.testService.getMany({})
       .pipe(
-        map(tests => tests.data),
-        shareReplay(1)
+        map(res => res.data),
+        shareReplay(1),
+        map(tests => tests.map(t => Object.assign(t, {disabled: this.isItemSelected(t)}))),
       );
+
+    this.searchTestText.valueChanges
+      .pipe(
+        auditTime(200),
+        tap(() => {
+          this.tests$ = this.testService.getMany({filter: {title: this.searchTestText.value.trim()}})
+            .pipe(
+              map(res => res.data),
+              shareReplay(1),
+              map(tests => tests.map(t => Object.assign(t, {disabled: this.isItemSelected(t)}))),
+            );
+          this.cdr.detectChanges();
+        }),
+      )
+      .subscribe();
+
+    this.searchArticleText.valueChanges
+      .pipe(
+        auditTime(200),
+        tap(() => {
+          this.articles$ = this.articleService.getMany({filter: {title: this.searchArticleText.value.trim()}})
+            .pipe(
+              map(res => res.data),
+              shareReplay(1),
+              map(tests => tests.map(t => Object.assign(t, {disabled: this.isItemSelected(t)}))),
+            );
+          this.cdr.detectChanges();
+        }),
+      )
+      .subscribe();
   }
 
   addArticle(): void {
     this.articleAddingMode = true;
+
   }
 
   addTest(): void {
     this.testAddingMode = true;
+  }
+
+  onSelectTest(test: Test): void {
+    this.testAddingMode = false;
+    this.items.push(this.fb.group({
+      id: this.fb.control(test.id),
+      title: this.fb.control(test.title),
+      type: this.fb.control(ItemType.TEST)
+    }));
+    this.selectedTest = null;
+  }
+
+  onSelectArticle(article: Article): void {
+    this.articleAddingMode = false;
+    this.items.push(this.fb.group({
+      id: this.fb.control(article.id),
+      title: this.fb.control(article.title),
+      type: this.fb.control(ItemType.ARTICLE)
+    }));
+    this.selectedArticle = null;
+  }
+
+  private isItemSelected(test: SimpleTest | Article): boolean {
+    return this.items.controls.some(c => c.get('id').value === test.id);
   }
 
 }
