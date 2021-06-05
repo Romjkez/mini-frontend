cf3df98b437d1596c39625e6c4c29bf55ec102ba
Romjkez <meshkov.ra@ya.ru>
2021-06-05 14:16:23 +0300

    fix(routing): fix paths

--- src/app/modules/exercises/exercises-routing.module.ts
+++ src/app/modules/exercises/exercises-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExercisesListComponent } from './components/exercises-list/exercises-list.component';
+import { ExerciseViewComponent } from './components/exercise-view/exercise-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  component: ExercisesListComponent,
+},
+  {
+    path: ':id',
+    pathMatch: 'full',
+    component: ExerciseViewComponent,
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/exercises/exercises.module.ts
+++ src/app/modules/exercises/exercises.module.ts
@@ -3,16 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { ExercisesRoutingModule } from './exercises-routing.module';
 import { ExercisesListComponent } from './components/exercises-list/exercises-list.component';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { SharedModule } from '../shared/shared.module';
+import { ExerciseService } from './exercise.service';
+import { ExerciseViewComponent } from './components/exercise-view/exercise-view.component';
 
 
 @NgModule({
   declarations: [
-    ExercisesListComponent
+    ExercisesListComponent,
+    ExerciseViewComponent
   ],
   imports: [
     CommonModule,
-    ExercisesRoutingModule
-  ]
+    ExercisesRoutingModule,
+    TableModule,
+    ButtonModule,
+    SharedModule
+  ],
+  providers: [ExerciseService]
 })
 export class ExercisesModule {
 }
